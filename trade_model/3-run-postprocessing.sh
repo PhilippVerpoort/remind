3d27647b9690caf6c2a4c2d36aa2a24434b9c9f3
Philipp Verpoort <philippv@pik-potsdam.de>
2021-07-29 13:20:05 +0200

    Added data export to csv file for  capacities.

--- trade_model/3-run-postprocessing.sh
+++ trade_model/3-run-postprocessing.sh
@@ -2,5 +2,4 @@
 
 # dump and convert import and export quantities from SSP2
 gdxdump ./output_data/results.gdx Symb=v24_shipment_quan | grep "'.L " | sed "s/'\.'/,/g" | sed "s/'.L /,/g" | sed "s/'//g" | sed "s/, //g" > ./output_data/shipmentquan.dat
-#gdxdump ./output_data/results.gdx Symb=vm_shipmentXadj | grep "'.L " | sed "s/'\.'/,/g" | sed "s/'.L /,/g" | sed "s/'//g" | sed "s/, //g" > ./output_data/shipmentXadj.dat
-#gdxdump ./output_data/results.gdx Symb=vm_shipmentMadj | grep "'.L " | sed "s/'\.'/,/g" | sed "s/'.L /,/g" | sed "s/'//g" | sed "s/, //g" > ./output_data/shipmentMadj.dat
+gdxdump ./output_data/results.gdx Symb=v24_cap_tradeTransp | grep "'.L " | sed "s/'\.'/,/g" | sed "s/'.L /,/g" | sed "s/'//g" | sed "s/, //g" > ./output_data/cap_tradeTransp.dat
